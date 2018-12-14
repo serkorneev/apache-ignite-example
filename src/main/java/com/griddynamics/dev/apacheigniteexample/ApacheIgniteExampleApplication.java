3da1973c874b405e2a5650071696b547e363d473
Sergey Korneev <skorneev@griddynamics.com>
2018-12-14 14:47:30 +0300

    Initial commit

---
+++ src/main/java/com/griddynamics/dev/apacheigniteexample/ApacheIgniteExampleApplication.java
@@ -0,0 +1,14 @@
+package com.griddynamics.dev.apacheigniteexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheIgniteExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheIgniteExampleApplication.class, args);
+	}
+
+}
+
